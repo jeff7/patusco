278cb11e509d2caa4af2a9ce3b2fbfd89c1e6372
jeff7 <jeffgribeiro@gmail.com>
2024-08-04 22:04:30 -0300

    snackbar componente

---
+++ resources/js/components/Snackbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-snackbar 
+    location="top" 
+    v-model="isVisible" 
+    :timeout="3000" 
+    :color="color">
+    {{ message }}
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      isVisible: this.show,
+    };
+  },
+  watch: {
+    show(newVal) {
+      this.isVisible = newVal;
+    },
+    isVisible(newVal) {
+      this.$emit('update:show', newVal);
+    },
+  },
+};
+</script>
